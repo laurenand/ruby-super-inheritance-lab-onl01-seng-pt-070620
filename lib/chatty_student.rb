0976ea156ca8d27daff107183063ec69a88a3197
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-15 18:05:19 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -9,24 +9,7 @@ class ChattyStudent < Student
   def raise_hand
     super
     puts "Pick me!"
-    super
-    puts "Pick me!"
-    super
-    puts "Pick me!"
-    super
-    puts "Pick me!"
-    super
-    puts "Pick me!"
-    super
-    puts "Pick me!"
-    super
-    puts "Pick me!"
-    super
-    puts "Pick me!"
-    super
-    puts "Pick me!"
-    super
-    puts "Pick me!"
+    
   end
   
 end
